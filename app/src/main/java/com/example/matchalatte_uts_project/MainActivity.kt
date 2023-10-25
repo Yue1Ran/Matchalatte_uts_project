08db8a6e9468edc87b2925c8bfe7d1730c482c22
Yue1Ran <s160418002@student.ubaya.ac.id>
2023-10-25 22:41:38 +0700

    nav bar

--- app/src/main/java/com/example/matchalatte_uts_project/MainActivity.kt
+++ app/src/main/java/com/example/matchalatte_uts_project/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.matchalatte_uts_project
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_main)
+        setContentView(R.layout.fragment_menu)
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment)
+                as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this,navController)
+        bottomNav.setupActionBarWithNavController(navController)
+
+
+
     }
 }
